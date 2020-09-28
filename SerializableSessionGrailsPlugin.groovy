f0edd284eecaf5c512fd84289ddbc19b61084e04
Dmitriy Drenkalyuk <dmitriy.drenkalyuk@secretescapes.com>
2020-09-28 16:16:49 +0100

    Updating versions.

--- SerializableSessionGrailsPlugin.groovy
+++ SerializableSessionGrailsPlugin.groovy
@@ -1,8 +1,8 @@
 class SerializableSessionGrailsPlugin {
     // the plugin version
-    def version = "0.5"
+    def version = "1.0.0"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.0 > *"
+    def grailsVersion = "2.5.4 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
